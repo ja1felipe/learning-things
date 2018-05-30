ae9a1d9b6129ba96d307f0b5e6d65295f71bcbe3
João Felipe <39709263+ja1felipe@users.noreply.github.com>
2018-05-30 18:42:44 -0300

    historico
    historico simples do lado

--- tkinter-calculator.py
+++ tkinter-calculator.py
@@ -13,7 +13,10 @@ def arruma(entrada):
 			nova += entrada[-1]
 	return nova
 
+
+
 def calculator():
+	historico = []
 	tudo = arruma(entrada.get())
 	tudo1 = tudo.split(' ')
 	x = float(tudo1[0])
@@ -27,17 +30,25 @@ def calculator():
 		resultado = x * y
 	if operador == '/':
 		resultado = x / y
-	lb['text'] = resultado
+	historia = tudo + ' = ' + str(resultado)
+	historico.append(historia)
+	lb2['text'] += historia + '\n'
+	lb['text'] = tudo + ' = ' + str(resultado)
+	
 
 janela = Tk()
-entrada = Entry(janela, width=15)
-entrada.place(x=135,y=120)
 
-lb = Label(janela, text='Resultado')
-lb.place(x=240,y=120)
+entrada = Entry(janela, width=15)
+entrada.place(x=100,y=120)
+
+lb = Label(janela, text='',)
+lb.place(x=100,y=140)
+
+lb2 = Label(janela, text='')
+lb2.place(x=225,y=120)
 
 bt = Button(janela, width=12, text='OK', command=calculator)
-bt.place(x=135,y=150)
+bt.place(x=100,y=160)
 
 
 
